8960c149c1723655036a1d181cdf63a1334bd1e7
Shane <40950903+shane654@users.noreply.github.com>
2023-03-08 21:06:51 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '785e51f85d834a448022b1731c8c70d0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://shane22.notion.site/Muketool-785e51f85d834a448022b1731c8c70d0',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
